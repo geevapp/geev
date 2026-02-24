dea7e624201113c28e686a3bf776efa2f31b12ac
OlufunbiIK <olufunbiibrahim@gmail.com>
2026-02-25 00:22:37 +0100

    Mutual Aid Request Creation #134

--- contracts/geev-core/src/types.rs
+++ contracts/geev-core/src/types.rs
@@ -18,6 +18,8 @@ pub enum Error {
     AlreadyInitialized = 12,
     ArithmeticOverflow = 13, // Added this to resolve the Clippy/Compile error
     NotAdmin = 14,
+InvalidGoalAmount = 15,
+HelpRequestAlreadyExists = 16,
 }
 
 #[derive(Clone, PartialEq, Eq)]
@@ -75,3 +77,5 @@ pub enum DataKey {
     Admin,
     Fee,
 }
+
+
